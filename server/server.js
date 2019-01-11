5ea1d6d9c01e7e88c4e816d4a8dda1314b078ea8
Yavrumian <vaheyavrumyan@gmail.com>
2019-01-11 13:07:31 +0400

    changed package.json

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const publicPath = path.join(__dirname, '../public');
+const port = PROCESS.ENV.port || 8080
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
@@ -11,6 +12,6 @@ app.use(express.static(publicPath));
 
 
 
-app.listen(8080, () =>{
-  console.log('working');
+app.listen(port, () =>{
+  console.log(`Running on port: ${port}`);
 })
